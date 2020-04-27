1feb959962c75b0945b55a8d3a04ead6b6c1e0eb
Hussein Fakharany <fakharany.hussein@gmail.com>
2020-04-27 12:12:56 +0200

    Added .html .txt filter

--- traducteurHTML.c
+++ traducteurHTML.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "analyse_syntaxique.h"
 #include <windows.h>
+#include "analyse_syntaxique.h"
 
 
 int main (int argc, char *argv[]) {
@@ -12,7 +12,18 @@ int main (int argc, char *argv[]) {
         printf("Il faut donner deux noms de fichiers en argument");
         exit(0);
     }
-
+    for(int i=strlen(argv[1])-4,j=0; i<strlen(argv[1]),j<strlen(".txt"); i++, j++){
+        if((argv[1][i])!=".txt"[j]) {
+            printf("Un fichier .txt est attendu\n");
+            exit(1);
+        }
+    }
+    for(int i=strlen(argv[2])-5,j=0; i<strlen(argv[2]),j<strlen(".html"); i++, j++) {
+        if ((argv[2][i]) != ".html"[j]) {
+            printf("Un fichier .html est attendu\n");
+            exit(1);
+        }
+    }
     analyser(argv[1],argv[2]);
     ShellExecute(NULL,"open",argv[2],NULL,NULL,SW_SHOWNORMAL);
     return 0 ;
